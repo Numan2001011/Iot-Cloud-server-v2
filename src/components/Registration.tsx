3527e2eb11ea83e9b2bed31047594bf6ef8b0ae0
Md. Numanur Rahman <2001011@iot.bdu.ac.bd>
2024-11-29 16:33:27 +0600

    Creating Project

---
+++ src/components/Registration.tsx
@@ -0,0 +1,14 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FieldValues } from "react-hook-form";
+import { z } from "zod";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Registration = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Registration
